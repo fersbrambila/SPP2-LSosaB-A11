62586d7917652fd2e44429fed3417d6709dddeb2
Fernanda <Fernanda@192.168.1.66>
2017-03-14 21:27:17 -0600

    Actividad 11 Luisa Fernanda Sosa Brambila A01411485 IIS

---
+++ src/spp2/lsosab/a11/SPP2LSosaBA11.java
@@ -0,0 +1,23 @@
+/*
+ * Luisa Fernanda Sosa Brambila
+ * A01411485
+ *IIS
+ */
+package spp2.lsosab.a11;
+
+/**
+ *
+ * @author Fernanda
+ */
+public class SPP2LSosaBA11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ventana1 Ventana2ProfeperdonMeEquivoqueAlCrearLaClaseBye= new ventana1();
+        ventana3 Ventana3aEsteSiSeLaPuseBien= new ventana3();
+    }
+    
+}
